4665124a0e456d00ef9f390c3fdb32b3ef1c7e46
Caleb Musau <calebmuteti4@gmail.com>
2024-10-02 15:01:32 +0300

    Confirm

--- admindash/confirm.php
+++ admindash/confirm.php
@@ -19,6 +19,7 @@ error_reporting(1);
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
+
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -32,6 +33,7 @@ error_reporting(1);
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
+
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 
@@ -42,6 +44,7 @@ error_reporting(1);
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
 </head>
+
 
 <body>
 
@@ -138,5 +141,4 @@ error_reporting(1);
     border-radius: 50%;
   }
 </style>
-
 </html>
